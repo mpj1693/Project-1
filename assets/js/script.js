d101fbd58145f5e57047cae8fe100deb17c359c5
Isabella Torres <izzy612@gmail.com>
2019-04-15 13:59:38 -0400

    layout

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+
+  $(document).ready(function () {
+  $(".search-button").click(function () {
+    console.log(".search-button")
+    
+    $(".zip-code").hide();
+
+    $(".spotify-button").show();
+  
+    $(".spotify-button").click(function () {
+
+      $(".spotify-button").hide();
+
+      $(".login-spotify").show();
+
+      $(".login-button").click(function () {
+
+        $(".lil-header").hide();
+        
+
+      })
+
+     })
+    
+    });
+    
+    
+  });
+
+  
+
+  
+  
+
+
+
+
+
+
+
+
+
