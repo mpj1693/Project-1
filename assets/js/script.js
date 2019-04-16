690db3edd538b847bf3715c11b01ad991cd3efed
Isabella Torres <izzy612@gmail.com>
2019-04-16 13:02:26 -0400

    ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,29 @@
     $(".zip-code").hide();
 
     $(".spotify-button").show();
+    
+   
   
     $(".spotify-button").click(function () {
 
       $(".spotify-button").hide();
 
       $(".login-spotify").show();
+      
+      var loginSpotify = $(".login-spotify");
+  
+      TweenLite.from(loginSpotify, 1, { x: -200 },{x:200});
 
-      $(".login-button").click(function () {
 
-        $(".lil-header").hide();
+        $(".login-button").click(function () {
+
+          $(".lil-header").hide();
+          
+          var spoTicket = $(".spotify-ticketmaster");
+
+          TweenLite.from(spoTicket, 1, { opacity: 0, y: 50 });
         
+          $(".spotify-ticketmaster").show();
 
       })
 
@@ -27,9 +39,16 @@
     
   });
 
-  
 
-  
+var header = $(".header");
+
+    lilHeader = $(".lil-header");
+
+        TweenLite.from(header, 1, {x: -200, ease:Power2.easeOut });
+TweenLite.from(lilHeader, 1, { autoAlpha: 0, delay: 1 });
+
+       
+
   
 
 
